52ca2a937e99cbe8704dc31c69759769bcf6ac76
justin1508 <justin1508@hotmail.com>
2022-05-13 08:10:58 -0300

    memo, virtualized and bundleAnalyzer

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,21 +1,51 @@
-import { memo } from "react";
+import { memo, useState, lazy } from "react";
+// import { AddProductToWishlist } from "./AddProductToWishlist";
+import { AddProductToWishlistProps } from "./AddProductToWishlist";
+import dynamic from 'next/dynamic';
+import lodash from 'lodash';
 
+const AddProductToWishlist = dynamic<AddProductToWishlistProps>(() => {
+	return import("./AddProductToWishlist").then(modulo => modulo.AddProductToWishlist)
+}, {
+	loading: () => <span>Carregando...</span>
+});
 interface ProductItemProps {
 	product: {
 		id: number;
 		price: number;
+		priceFormatted: string;
 		title: string;
-	}
+	},
+	onAddToWishlist: (id: number) => void;
 }
 
-export function ProductItemComponent({ product }: ProductItemProps) {
+export function ProductItemComponent({ product, onAddToWishlist }: ProductItemProps) {
+	const [isAddingToWishlist, setIsAddingToWishlist] = useState(false);
+
+	// async function showFormattedDate() {
+	// 	const { format } = await import('date-fns')
+
+	// 	format()
+	// }
+
 	return (
 		<div>
-			{product.title} - <strong>{product.price}</strong>
+			{product.title} - <strong>{product.priceFormatted}</strong>
+			{/* <button onClick={() => onAddToWishList(product.id)}>Add to wish list</button> */}
+			<button onClick={() => setIsAddingToWishlist(true)}>Add to wish list</button>
+
+			{isAddingToWishlist &&
+				<AddProductToWishlist
+					onAddToWishlist={() => onAddToWishlist(product.id)}
+					onRequestClose={() => setIsAddingToWishlist(false)}
+				/>
+			}
+
 		</div>
 	)
 }
 
 export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
-	return Object.is(prevProps.product, nextProps.product);
+	// return Object.is(prevProps.product, nextProps.product);
+	return lodash.isEqual(prevProps.product, nextProps.product);
 });
--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,22 +1,57 @@
+// import { useMemo } from "react"
+import { List, ListRowRenderer } from "react-virtualized"
 import { ProductItem } from "./ProductItem"
-
 interface SearchResultsProps {
+	totalPrice: number;
 	results: Array<{
 		id: number;
 		price: number;
+		priceFormatted: string;
 		title: string;
 	}>
+	onAddToWishList: (id: number) => void;
 }
 
+export function SearchResults({ results, onAddToWishList, totalPrice }: SearchResultsProps) {
 
-export function SearchResults({ results }: SearchResultsProps) {
+	// const totalPrice = useMemo(() => {
+	// 	return results.reduce((total, product) => {
+	// 		return total + product.price;
+	// 	}, 0)//comeca em zero
+	// }, [results]);
+
+	const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+		return (
+			<div key={key} style={style}>
+				<ProductItem
+					product={results[index]}
+					onAddToWishlist={onAddToWishList}
+				/>
+			</div>
+		)
+	}
+
 	return (
 		<div>
-			{results.map(product => {
+			<h2>{totalPrice}</h2>
+
+			{/* {results.map(product => {
 				return (
-					<ProductItem product={product} key={product.id} />
+					<ProductItem
+						product={product}
+						key={product.id}
+						onAddToWishlist={onAddToWishList}
+					/>
 				)
-			})}
+			})} */}
+			<List
+				height={300}
+				rowHeight={30}
+				width={900}
+				overscanColumnCount={5}
+				rowCount={results.length}
+				rowRenderer={rowRenderer}
+			/>
 		</div>
 	)
 }
